818e557bb48d0c03f3e08611f9f65ec8cb663a4a
Israel Torres <IsraelTorres@users.noreply.github.com>
2017-04-20 18:05:05 -0700

    counts down to a specific time period when started
    regardless of when the shell file begins, it will calculate the current time versus the target time and countdown in seconds; options for smooth single line or standard scrolling...

---
+++ countdown-to-target-time.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Israel Torres
+# 20170420 - v5 (gTfo)
+# ./countdown-to-target-time.sh
+# 4:30PM countdown timer
+# counts down to (target time) 4:30pm in seconds regardless 
+# of what time the shell file is started.
+# special shoutz to ph4nt4zm & trash1e
+#
+# time_target=$(( 16 * 3600 + 30 * 60 )) # 59400, 16:30, 4:30PM
+#
+while [[ 1 ]]
+	do
+	# added checks so null errors don't come up
+	cHr=$(date +%H); fxdcHr=$(echo $((10#$cHr)) )
+	cMn=$(date +%M); fxdcMn=$(echo $((10#$cMn)) )
+	cSc=$(date +%S); fxdcSs=$(echo $((10#$cSc)) )
+	# no more need for playful second (+1)
+	time_countd=$(( $fxdcHr * 3600 + $fxdcMn * 60 + $fxdcSs ))
+		if [[ "$time_countd" -ge "$time_target" ]]
+			then
+				echo -en "It's 4:30PM, GTFO!\n"
+				exit 0
+			else
+				countdown=$(($time_target - $time_countd))
+				#echo -ne "gTfo-$countdown\n" # scrolling style
+				echo -en "\033[2K\r"       #single line style 1 of 2
+				echo -en "gTfo-$countdown" #single line style 2 of 2
+		fi
+		sleep 1
+	done
+#EOF
